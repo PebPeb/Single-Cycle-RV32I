f82bad6dbae72d1d49aa03ee576caa0753f3273a
PebPeb <bmkeen@liberty.edu>
2022-10-11 00:05:55 -0500

    Added Extend S-Type
    Should be sign extended not working currently

--- extend.v
+++ extend.v
@@ -12,7 +12,7 @@
 
 // Change Log:	NA
 
-module extend(Instr, ImmSrc, ExtImm)
+module extend(Instr, ImmSrc, ExtImm);
 	input wire [1:0]	ImmSrc;
 	input wire [24:0]	Instr;
 	output reg [31:0]	ExtImm;
@@ -20,7 +20,8 @@ module extend(Instr, ImmSrc, ExtImm)
 	
 	always @(ImmSrc, Instr) begin
 		case (ImmSrc)
-			2'b00:	
+			2'b00:	ExtImm <= {Instr[24:5], 12'h000};						// U-Type
+			2'b01:	ExtImm <= {Instr[24:18], Instr[4:0], 20'h00000} >>> 20;	// S-Type
 			default:ExtImm <= 32'h00000000;
 		endcase
 	end
---
+++ sims/extend/extend_tb.v
@@ -0,0 +1,37 @@
+
+//
+//	extend_tb.v
+//		Immediate Extension Testbentch
+//
+
+// -------------------------------- //
+//	By: Bryce Keen	
+//	Created: 10/10/2022
+// -------------------------------- //
+//	Last Modified: 10/10/2022
+
+// Change Log:	NA
+
+module extend_tb();
+	reg [1:0]		ImmSrc;
+	reg [31:0]		Instruction;
+	wire [31:0]		ExtImm;
+
+
+	extend UUT(
+		.ImmSrc(ImmSrc),
+		.Instr(Instruction[31:7]),
+		.ExtImm(ExtImm)
+		);
+
+	initial begin
+		ImmSrc <= 2'b00;
+		Instruction <= 32'h0FF00FF0;
+		#2
+		ImmSrc <= 2'b01;
+		Instruction <= 32'hFFFFFF80;
+		#2
+		$finish;
+	end
+
+endmodule
