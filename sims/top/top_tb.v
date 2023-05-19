11dc6cc1c662d6dfff6649d993e66c0863c225b3
PebPeb <67885698+PebPeb@users.noreply.github.com>
2023-05-18 23:50:36 -0400

    Small Changes

--- sims/top/top_tb.v
+++ sims/top/top_tb.v
@@ -10,18 +10,25 @@ module top_tb();
         #2;
     end
 
-    Top UUT (.clk(clk), .reset(reset));
+    Top DUT (.clk(clk), .reset(reset));
 
     initial begin
         reset <= 1;
         #4;
         reset <= 0;
-        #12
+        #220
         $finish;
     end
 
+    integer i;
 	initial begin
 		$dumpfile("top_tb.vcd");
 		$dumpvars(0, top_tb);
+        //for (i = 0; i < 31; i = i + 1)
+        //    $dumpvars(1, DUT.RV32I_Logic.Signle_Cycle_Datapath.regFILE.x[i]);
+        
+        for (i = 0; i < 32; i = i + 1)
+            $dumpvars(1, DUT.RV32I_Logic.Single_Cycle_Datapath.regFILE.x[i]);
+
 	end
 endmodule
